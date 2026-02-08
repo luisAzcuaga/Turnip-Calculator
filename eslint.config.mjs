1913672670f9a323d01fe5242ed5864391fd4b58
Luis Azcuaga <37643667+luisAzcuaga@users.noreply.github.com>
2026-02-07 18:38:49 -0600

    feat: add sort import rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,11 +5,19 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["eslint.config.mjs","node_modules/**", "dist/**"],
     plugins: { js }, extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
     rules: {
       "no-case-declarations": "off",
       "no-unused-vars": ["warn"],
+      "sort-imports": ["warn", {
+        "ignoreCase": false,
+        "ignoreDeclarationSort": false,
+        "ignoreMemberSort": false,
+        "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+        "allowSeparatedGroups": false
+      }],
     },
   }
 ]);
